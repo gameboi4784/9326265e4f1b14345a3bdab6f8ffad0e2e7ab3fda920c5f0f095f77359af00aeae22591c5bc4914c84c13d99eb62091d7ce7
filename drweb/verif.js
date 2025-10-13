47faded851d6ca7e3ad756b18f97e2da01726cea
1e295a49108e716ce8ab7eba0c8dca7d <89362751+1e295a49108e716ce8ab7eba0c8dca7d@users.noreply.github.com>
2025-10-13 05:43:45 -0700

    ah

--- drweb/verif.js
+++ drweb/verif.js
@@ -9,7 +9,7 @@
         alert("Loading, this may take a while...");
         alert("Make sure to click inside the tab when the game finishes loading for audio!");
 
-      const res = await fetch(`https://cdn.jsdelivr.net/gh/1e295a49108e716ce8ab7eba0c8dca7d/wowzas45@main/drweb/verif.html?t=${Date.now()}`);
+      const res = await fetch(`https://cdn.jsdelivr.net/gh/1e295a49108e716ce8ab7eba0c8dca7d/wowzas47@main/drweb/verif.html?t=${Date.now()}`);
       const html = await res.text();
 
       const newTab = window.open('about:blank');
--- nutes.py
+++ nutes.py
@@ -17,4 +17,4 @@ def replace_in_files(root_dir, old_text, new_text):
                     print(f"Error processing {file_path}: {e}")
 
 if __name__ == "__main__":
-    replace_in_files(".", "wowzas45", "wowzas46")
+    replace_in_files(".", "wowzas46", "wowzas47")
