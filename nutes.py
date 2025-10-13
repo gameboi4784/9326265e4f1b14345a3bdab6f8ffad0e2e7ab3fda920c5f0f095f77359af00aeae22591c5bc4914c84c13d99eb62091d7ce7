46af52648ad0f8ff019c35ff13a314f1bce3b61e
1e295a49108e716ce8ab7eba0c8dca7d <89362751+1e295a49108e716ce8ab7eba0c8dca7d@users.noreply.github.com>
2025-10-13 03:26:41 -0700

    ah

--- nutes.py
+++ nutes.py
@@ -17,4 +17,4 @@ def replace_in_files(root_dir, old_text, new_text):
                     print(f"Error processing {file_path}: {e}")
 
 if __name__ == "__main__":
-    replace_in_files(".", "wowzas31", "wowzas32")
+    replace_in_files(".", "wowzas32", "wowzas33")
