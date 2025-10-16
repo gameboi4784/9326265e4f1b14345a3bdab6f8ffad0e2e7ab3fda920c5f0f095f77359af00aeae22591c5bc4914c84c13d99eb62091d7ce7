45ea516c77654d1db2ab0c8bae474a08c2b70d21
1e295a49108e716ce8ab7eba0c8dca7d <89362751+1e295a49108e716ce8ab7eba0c8dca7d@users.noreply.github.com>
2025-10-16 02:10:56 -0700

    ‌

--- nutes.py
+++ nutes.py
@@ -17,4 +17,4 @@ def replace_in_files(root_dir, old_text, new_text):
                     print(f"Error processing {file_path}: {e}")
 
 if __name__ == "__main__":
-    replace_in_files(".", "wowzas49", "")
+    replace_in_files(".", "ca7d/@main", "ca7d/9326265e4f1b14345a3bdab6f8ffad0e2e7ab3fda920c5f0f095f77359af00aeae22591c5bc4914c84c13d99eb62091d7ce6@main")
